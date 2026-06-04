6c197eb20acde163b4b7fd74d8d50064249bc7e6
payment-processing-servi-admin <agrimadixit13@gmail.com>
2026-06-04 11:16:20 +0530

    completed Location-Service code ,full coding for how to find nearby drivers within given radius,completed Redis configuration,setting up application.properties

--- location-service/src/main/java/com/rideshare/location_service/config/RedisConfig.java
+++ location-service/src/main/java/com/rideshare/location_service/config/RedisConfig.java
@@ -1,5 +1,25 @@
 package com.rideshare.location_service.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
 public class RedisConfig {
  
+	@Bean
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+		// Configure the Redis connection factory and other settings as needed
+		return redisTemplate;
+	}
+	
 }
--- location-service/src/main/java/com/rideshare/location_service/service/LocationService.java
+++ location-service/src/main/java/com/rideshare/location_service/service/LocationService.java
@@ -1,11 +1,20 @@
 package com.rideshare.location_service.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jspecify.annotations.Nullable;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Metrics;
 import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.rideshare.location_service.dto.DriverLocationRequest;
+import com.rideshare.location_service.dto.NearByDriverResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -45,9 +54,63 @@ public class LocationService {
 		log.info("Driver location updated in Redis for driver: {}", request.getDriverId());
 	  }
 
-	  public @Nullable Object findNearbyDrivers(double latitude, double longitude, double radiusInKm) {
-		// TODO Auto-generated method stub
-		return null;
+	  /**
+	   * find nearby drivers within a certain radius of a given location
+	   * called by matching service on ride request to find nearby drivers
+	   * maps to redis GEORADIUS command to query the geospatial index for drivers within a specified radius of the given location.
+	   */
+	  
+	  public List<NearByDriverResponse> findNearbyDrivers(
+			  double latitude, 
+			  double longitude, 
+			  double radiusInKm
+			  ) 
+	  {
+		  log.info("Finding nearby drivers for location: lat {}, lon {}, radius {} km", latitude, longitude, radiusInKm);
+		  //the Circle class represents a circular area defined by a center point and a radius. The Point class represents a geographical point defined by its longitude and latitude. 
+		  //The Distance class represents a distance value along with its unit of measurement (in this case, kilometers).
+		  Circle searchArea = new Circle(new Point(longitude, latitude), new Distance(radiusInKm,Metrics.KILOMETERS)); // Convert radius to radians (Earth's radius in km)
+		  
+		  GeoResults<RedisGeoCommands.GeoLocation<String>> geoResults = redisTemplate.opsForGeo().radius(DRIVER_LOCATION_KEY, 
+				  searchArea,
+				  RedisGeoCommands.GeoRadiusCommandArgs.newGeoRadiusArgs()
+				  .includeCoordinates()
+				  .includeDistance()
+				  .sortAscending()
+				  .limit(10)
+				  );
+		  
+		  List<NearByDriverResponse> nearbyDrivers = new ArrayList<>();
+		  
+		  if(geoResults != null) {
+			  geoResults.forEach(result -> {
+				 RedisGeoCommands.GeoLocation<String> location = result.getContent();
+				 nearbyDrivers.add(new NearByDriverResponse(
+						 location.getName(), // driverId
+						 location.getPoint().getY(), // latitude
+						 location.getPoint().getX(), // longitude
+						 result.getDistance().getValue() // distance in km
+						 ));
+			  });
+		  }
+		  
+		  log.info("Found {} nearby drivers", nearbyDrivers.size());
+		  
+		  return nearbyDrivers;
+	  
+	 
+}
+	  
+	 /**
+	  * remove driver when they go offline or log out
+	  * maps to redis ZREM command to remove the driver's location from the geospatial index.
+	  * 
+	  */
+	  
+	  public void removeDriver(String driverId) {
+		  log.info("Removing location for driver: {}", driverId);
+		  redisTemplate.opsForGeo().remove(DRIVER_LOCATION_KEY, driverId);
+		  log.info("Driver location removed from Redis for driver: {}", driverId);
 	  }
-
+	  
 }
